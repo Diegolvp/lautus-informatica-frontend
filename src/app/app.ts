9bde597dcd39a999e608ad2c032d21dd13438f41
Diego Pievan <diego.pievan123@gmail.com>
2025-11-09 22:13:38 -0300

    fix: app only router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SideMenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  
   protected readonly title = signal('lautus-informatica');
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 }
